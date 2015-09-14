f1a0a11e07d5a2174cec441e4e6f6e8813340c1f
ilyatoka <ilyatoka@yahoo.com>
2015-09-14 22:55:10 +0300

    Init repo. Add empty C project
    First initialise commit with basic setup of C project

---
+++ LCHlearnchardway/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  LCHlearnchardway
+//
+//  Created by ILYA Toka on 9/14/15.
+//  Copyright (c) 2015 ILYA Toka. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
