7637afbb3970e08debf15612802abff066575342
ilyatoka <ilyatoka@yahoo.com>
2015-10-01 23:16:59 +0300

    Refactor tests for Structures

--- Sources/LCHPerformStructures/LCHPerformStructures.h
+++ Sources/LCHPerformStructures/LCHPerformStructures.h
@@ -1,57 +1,23 @@
 #ifndef LCHPerformStructures_h
 #define LCHPerformStructures_h
 
-#include <stdio.h>
-#include <stddef.h>
 #include <stdbool.h>
 
-#define LCHPrintTextForOffsetOfStructure(structure, structureElement) \
-    printf("\tOffset of " #structureElement " is: %lu\n", offsetof(structure, structureElement));
-
-#define LCHPrintOffsetOfStructureElements(structure) \
-    LCHPrintTextForOffsetOfStructure(structure, bool1) \
-    LCHPrintTextForOffsetOfStructure(structure, bool2) \
-    LCHPrintTextForOffsetOfStructure(structure, bool3) \
-    LCHPrintTextForOffsetOfStructure(structure, bool4) \
-    LCHPrintTextForOffsetOfStructure(structure, bool5) \
-    LCHPrintTextForOffsetOfStructure(structure, bool6) \
-    LCHPrintTextForOffsetOfStructure(structure, doubleValue) \
-    LCHPrintTextForOffsetOfStructure(structure, shortValue1) \
-    LCHPrintTextForOffsetOfStructure(structure, floatValue) \
-    LCHPrintTextForOffsetOfStructure(structure, shortValue2) \
-    LCHPrintTextForOffsetOfStructure(structure, shortValue3) \
-    LCHPrintTextForOffsetOfStructure(structure, intValue) \
-    LCHPrintTextForOffsetOfStructure(structure, longLongValue) \
-    LCHPrintTextForOffsetOfStructure(structure, string) \
-    printf("\n");
-
-#define LCHPrintOffsetOfUnionElements(structure) \
-    LCHPrintTextForOffsetOfStructure(structure, string) \
-    LCHPrintTextForOffsetOfStructure(structure, longLongValue) \
-    LCHPrintTextForOffsetOfStructure(structure, doubleValue) \
-    LCHPrintTextForOffsetOfStructure(structure, floatValue) \
-    LCHPrintTextForOffsetOfStructure(structure, intValue) \
-    LCHPrintTextForOffsetOfStructure(structure, shortValue1) \
-    LCHPrintTextForOffsetOfStructure(structure, shortValue2) \
-    LCHPrintTextForOffsetOfStructure(structure, shortValue3) \
-    LCHPrintTextForOffsetOfStructure(structure, boolFlags) \
-    printf("\n");
-
 typedef struct {
+    char *string;
     bool bool1;
     bool bool2;
     double doubleValue;
     bool bool3;
     bool bool4;
+    long long longLongValue;
     bool bool5;
-    bool bool6;
     short shortValue1;
+    bool bool6;
     float floatValue;
     short shortValue2;
     short shortValue3;
     int intValue;
-    long long longLongValue;
-    char *string;
 } LCHRandomStructure;
 
 typedef struct {
--- Tests/LCHPerformStructuresTests/LCHPerformStructuresTests.c
+++ Tests/LCHPerformStructuresTests/LCHPerformStructuresTests.c
@@ -1,17 +1,48 @@
+#include <stdio.h>
+#include <stddef.h>
+
 #include "LCHPerformStructuresTests.h"
 #include "LCHPrintSizeOfDataType.h"
 #include "LCHPerformStructures.h"
 
 #pragma mark -
+#pragma mark Private Declarations
+
+#define LCHPrintTextForOffsetOfStructure(structure, structureElement) \
+printf("\tOffset of " #structureElement " is: %lu\n", offsetof(structure, structureElement));
+
+#define LCHPrintOffsetOfStructureBoolElements(structure) \
+    LCHPrintTextForOffsetOfStructure(structure, bool1) \
+    LCHPrintTextForOffsetOfStructure(structure, bool2) \
+    LCHPrintTextForOffsetOfStructure(structure, bool3) \
+    LCHPrintTextForOffsetOfStructure(structure, bool4) \
+    LCHPrintTextForOffsetOfStructure(structure, bool5) \
+    LCHPrintTextForOffsetOfStructure(structure, bool6)
+
+#define LCHPrintOffsetOfStructureElements(structure) \
+    LCHPrintTextForOffsetOfStructure(structure, doubleValue) \
+    LCHPrintTextForOffsetOfStructure(structure, shortValue1) \
+    LCHPrintTextForOffsetOfStructure(structure, floatValue) \
+    LCHPrintTextForOffsetOfStructure(structure, shortValue2) \
+    LCHPrintTextForOffsetOfStructure(structure, shortValue3) \
+    LCHPrintTextForOffsetOfStructure(structure, intValue) \
+    LCHPrintTextForOffsetOfStructure(structure, longLongValue) \
+    LCHPrintTextForOffsetOfStructure(structure, string) \
+    printf("\n");
+
+#define LCHPerformStructuresTestsNamed(structure) \
+    LCHPrintSizeOfDataType(structure); \
+    LCHPrintOffsetOfStructureBoolElements(structure); \
+    LCHPrintOffsetOfStructureElements(structure);
+
+#pragma mark -
 #pragma mark Public Implementations
 
 void LCHPerfomStructuresTests(void) {
-    LCHPrintSizeOfDataType(LCHRandomStructure);
-    LCHPrintOffsetOfStructureElements(LCHRandomStructure);
-    
-    LCHPrintSizeOfDataType(LCHOptimizedStructure);
-    LCHPrintOffsetOfStructureElements(LCHOptimizedStructure);
+    LCHPerformStructuresTestsNamed(LCHRandomStructure);
+    LCHPerformStructuresTestsNamed(LCHOptimizedStructure);
     
     LCHPrintSizeOfDataType(LCHOptimizedUnion);
-    LCHPrintOffsetOfUnionElements(LCHOptimizedUnion);
+    LCHPrintTextForOffsetOfStructure(LCHOptimizedUnion, boolFlags);
+    LCHPrintOffsetOfStructureElements(LCHOptimizedUnion);
 }
