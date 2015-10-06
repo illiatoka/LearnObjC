e92bda704011c6333bd9341b43e1da95964dddc9
ilyatoka <ilyatoka@yahoo.com>
2015-10-06 15:34:57 +0300

    Move struct definition to public interface

--- Sources/LCHHumanObject/LCHHumanObject.c
+++ Sources/LCHHumanObject/LCHHumanObject.c
@@ -5,14 +5,5 @@
 #pragma mark -
 #pragma mark Private Declarations
 
-struct LCHHuman {
-    char *_name;
-    char *_surname;
-    LCHHuman *_partner;
-    LCHHuman *_mother;
-    LCHHuman *_father;
-    LCHHuman *_children[20];
-    uint8_t _age;
-    uint8_t _childrenCount;
-    LCHHumanGenderType _gender;
-};
+#pragma mark -
+#pragma mark Public Implementations
--- Sources/LCHHumanObject/LCHHumanObject.h
+++ Sources/LCHHumanObject/LCHHumanObject.h
@@ -8,6 +8,18 @@ typedef enum {
     LCHHumanGenderFemale
 } LCHHumanGenderType;
 
+struct LCHHuman {
+    char *_name;
+    char *_surname;
+    LCHHuman *_partner;
+    LCHHuman *_mother;
+    LCHHuman *_father;
+    LCHHuman *_children[20];
+    uint8_t _age;
+    uint8_t _childrenCount;
+    LCHHumanGenderType _gender;
+};
+
 extern
 LCHHuman *LCHHumanCreate(void);
 
