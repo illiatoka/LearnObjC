738c44d70b07008d3cc0076df9456fcacf9173e2
ilyatoka <ilyatoka@yahoo.com>
2015-12-03 17:24:43 +0200

    Test initWithRange method

--- ProjectObjCTests/Specs/LCHAlphabetSpec.m
+++ ProjectObjCTests/Specs/LCHAlphabetSpec.m
@@ -10,13 +10,23 @@ SPEC_BEGIN(LCHAlphabetSpec)
 describe(@"LCHAlphabet", ^{
     __block LCHAlphabet *alphabet = nil;
     
+    afterEach(^{
+        alphabet = nil;
+    });
+    
     context(@"when initialized with +alphabetWithRange:('A' - 'G')", ^{
         beforeAll(^{
             alphabet = [LCHAlphabet alphabetWithRange:NSMakeRange('A', 'G' - 'A')];
         });
         
-        afterAll(^{
-            alphabet = nil;
+        it(@"should be of Class LCHRangeAlphabet", ^{
+            [[alphabet should] beKindOfClass:[LCHRangeAlphabet class]];
+        });
+    });
+    
+    context(@"when initialized with -initWithRange:('A' - 'G')", ^{
+        beforeAll(^{
+            alphabet = [[LCHAlphabet alloc ] initWithRange:NSMakeRange('A', 'G' - 'A')];
         });
         
         it(@"should be of Class LCHRangeAlphabet", ^{
